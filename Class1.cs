13ea99784991aac14251d10fd9b827e1016fc2ae
Jan Holan <jan.holan@gymspit.cz>
2019-11-11 09:00:55 +0100

    Add real project

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    abstract class Animal
+    {
+
+        public virtual int getLegCount()
+        {
+            return 5;
+        }
+
+        abstract public void introduce2();
+    }
+
+    class Dog : Animal
+    {
+        private string name;
+        public int dogCount = 0;
+
+        public Dog(string name = "Vendelin")
+        {
+            this.name = name;
+            Console.WriteLine("Vyrabim psa");
+            dogCount++;
+        }
+
+        public override int getLegCount()
+        {
+            return 4;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return this.name.ToUpper();
+        }
+
+        public override void introduce2()
+        {
+            Console.WriteLine("WOOF " + name + " and my leg count is " + this.getLegCount());
+            Console.WriteLine("And count of dogs is " + dogCount);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication11;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.introduce2();
+            Console.ReadKey();
+        }
+    }
+}
